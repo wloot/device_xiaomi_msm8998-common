cf82463aab2e8526911615c019c10a9801529863
Vivek Vilvaraj <vvilvara@codeaurora.org>
2018-07-17 00:05:16 +0530

    msm8998: BoardConfig : Enable 64 bit DRM plugins compilation
    Enable 64 bit compilation for DRM plugins.
    This will generate DRM plugins, HALs & oemcrypto_test in 64 bit
    
    Change-Id: I620016aacfab9281a383bbfa44df9c3f75c7baff

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -235,5 +235,8 @@ WIFI_DRIVER_FW_PATH_P2P := "p2p"
 WIFI_DRIVER_OPERSTATE_PATH := "/sys/class/net/wlan0/operstate"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
+#Enable DRM plugins 64 bit compilation
+TARGET_ENABLE_MEDIADRM_64 := true
+
 # Inherit from proprietary files
 include vendor/xiaomi/msm8998-common/BoardConfigVendor.mk
