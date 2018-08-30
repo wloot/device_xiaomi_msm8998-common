1310ae8e22c424627f22ac58664f40ef4bed936b
wloot <wlootlxt123@gmail.com>
2018-08-31 00:39:54 +0800

    Revert "msm8998-common: disable shutter sound for miui cam"
    This reverts commit ea7b7768f45f7490cb1bea01cc528d87bc0745d6.

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -60,7 +60,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-	camera.shutter_sound.blacklist=com.android.camera \
 	persist.camera.HAL3.enabled=1 \
 	persist.camera.set.afd=4 \
 	persist.camera.xm.green.b=0.96 \
