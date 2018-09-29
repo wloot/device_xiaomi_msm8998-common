10c72d93e944102fcc3d5b5cb0d6e3315f7b98f2
wloot <wlootlxt123@gmail.com>
2018-09-30 02:43:11 +0800

    msm8998-common: Clean Snap configs

--- msm8998.mk
+++ msm8998.mk
@@ -126,10 +126,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
-# Camera
-PRODUCT_PACKAGES += \
-    Snap
-
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service
