6bebfd596d00140712cb53b84dace9167750c669
Han Wang <416810799@qq.com>
2018-10-02 05:17:10 +0000

    msm8998-common: Update textclassifier package name for P
    Change-Id: If846cc590060025202fa336af4420747f6b01238

--- msm8998.mk
+++ msm8998.mk
@@ -341,7 +341,7 @@ PRODUCT_PACKAGES += \
 
 # TextClassifier smart selection model files
 PRODUCT_PACKAGES += \
-    textclassifier.smartselection.bundle1
+    textclassifier.bundle1
 
 # Wifi
 PRODUCT_PACKAGES += \
